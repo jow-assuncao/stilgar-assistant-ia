b180b0bd77c682e39e251b42e80fe3ac304733e8
Jonathan Assunção <almeida_jn@hotmail.com>
2025-05-20 18:24:27 -0300

    remove unused code

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,7 +2,6 @@ import json
 import os
 
 HISTORY_PATH = "memory/history.json"
-INTERPRETOR_ROLE_PATH = "memory/interpretor.json"
 
 
 def load_history():
@@ -11,12 +10,7 @@ def load_history():
             return json.load(f)
     return []
 
+
 def save_history(history):
     with open(HISTORY_PATH, "w", encoding="utf-8") as f:
         json.dump(history, f, ensure_ascii=False, indent=2)
-        
-def load_interpretor_role():
-    if os.path.exists(INTERPRETOR_ROLE_PATH):
-        with open(INTERPRETOR_ROLE_PATH, "r", encoding="utf-8") as f:
-            return json.load(f)
-    return {}
